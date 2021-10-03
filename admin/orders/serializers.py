4292f4bd0b8a666ffced2541cbaa73e347482c29
Hammad Hassan <hhammad2017@gmail.com>
2021-10-03 17:45:32 +0500

    Added a method that computes the total price of order in orders serializer

--- admin/orders/serializers.py
+++ admin/orders/serializers.py
@@ -11,6 +11,12 @@ class OrderItemSerializer(serializers.ModelSerializer):
 
 class OrderSerializer(serializers.ModelSerializer):
     order_items = OrderItemSerializer(many=True)
+    total = serializers.SerializerMethodField('get_total')
+
+    # get total price
+    def get_total(self, obj):
+        items = OrderItem.objects.all().filter(order_id= obj.id)
+        return sum((o.price * o.quantity) for o in items)
 
     class Meta:
         model = Order
--- admin/orders/views.py
+++ admin/orders/views.py
@@ -3,7 +3,6 @@ from rest_framework import generics, mixins
 from rest_framework.permissions import IsAuthenticated
 
 from admin.pagination import CustomPagination
-from rest_framework.views import APIView
 
 from .models import Order
 from .serializers import OrderSerializer
@@ -12,9 +11,7 @@ from users.authentication import JWTAuthentication
 
 class OrderGenericAPIView(generics.GenericAPIView,
                          mixins.ListModelMixin,
-                         mixins.RetrieveModelMixin,
-                         mixins.CreateModelMixin,
-                         mixins.UpdateModelMixin):
+                         mixins.RetrieveModelMixin,):
 
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
@@ -25,7 +22,7 @@ class OrderGenericAPIView(generics.GenericAPIView,
     def get(self, request, pk=None):
         if pk:
             return Response({
-                'data': self.retrieve(request, pk)
+                'data': self.retrieve(request, pk).data
             })
         return self.list(request)
 
