b451d09b4bddf0de48ee660e81b063bccb98f43c
Hammad Hassan <hhammad2017@gmail.com>
2021-10-03 05:31:12 +0500

    Image Uploading completed

--- admin/products/urls.py
+++ admin/products/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ProductsGenericAPIView
+from .views import ProductsGenericAPIView, FileUploadView
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('products', ProductsGenericAPIView.as_view()),
-    path('products/<str:pk>', ProductsGenericAPIView.as_view())
-
-]
+    path('products/<str:pk>', ProductsGenericAPIView.as_view()),
+    path('upload', FileUploadView.as_view())
+] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
--- admin/products/views.py
+++ admin/products/views.py
@@ -1,9 +1,13 @@
+from django.core.files.storage import default_storage
 from django.shortcuts import render
+from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
 from rest_framework import generics, mixins
 from rest_framework.permissions import IsAuthenticated
 
 from admin.pagination import CustomPagination
+from rest_framework.views import APIView
+
 from .models import Product
 from .serializers import ProductSerializer
 from users.authentication import  JWTAuthentication
@@ -41,3 +45,18 @@ class ProductsGenericAPIView(generics.GenericAPIView,
 
     def delete(self, request, pk=None):
         return self.destroy(request, pk)
+
+
+class FileUploadView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    parser_classes = (MultiPartParser,)
+
+    def post(self, request):
+        file = request.FILES['image']
+        file_name = default_storage.save(file.name, file)
+        url = default_storage.url(file_name)
+
+        return Response({
+            'url': 'http://localhost:8000/api'+url
+        })
